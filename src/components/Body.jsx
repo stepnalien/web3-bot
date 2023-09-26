7b9e9f125b45116101eb3b52299b69c5849ac76f
ons dev <121960161+stepnalien@users.noreply.github.com>
2023-09-26 22:10:51 +0600

    initial commit

---
+++ src/components/Body.jsx
@@ -0,0 +1,9 @@
+function Body () {
+    return (
+        <>
+        <div className="body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis ab iste obcaecati error rerum, quas enim in quasi nulla nisi! Similique tempore nobis corporis consequuntur laboriosam dignissimos optio, cum inventore explicabo ratione velit ab ad magnam minima sunt soluta labore unde molestiae architecto cumque reprehenderit doloremque id? Nobis, corrupti. Neque vitae maxime distinctio saepe, natus, fugiat quia, impedit officiis officia sint quaerat commodi! Reprehenderit voluptas adipisci ut dicta iste, accusantium eos ea aliquam aspernatur cum provident officia neque a numquam recusandae, sunt vel nesciunt totam officiis. Enim expedita doloremque obcaecati, aperiam minus debitis omnis? Qui accusantium quod magnam porro nobis error illo quos molestiae, consectetur dolores exercitationem atque nemo unde fuga natus aut, laborum iusto tempore est quia fugit tempora deleniti quasi. Optio atque magnam porro similique eveniet magni maiores, et amet pariatur, ea explicabo. Ipsum minima, doloribus quis nemo perferendis eaque saepe a, similique corrupti temporibus, pariatur natus quam.</div>
+        </>
+    )
+}
+
+export default Body
